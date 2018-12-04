078899c1517bddab61cd530cba10981d7b1b6eda
loic.stevens <loic.stevens.etu@univ-lille.fr>
2018-12-04 17:13:28 +0100

    Push magistral

--- src/main/scala/sid/SimpleApp.scala
+++ src/main/scala/sid/SimpleApp.scala
@@ -2,12 +2,22 @@ import org.apache.spark.sql.SparkSession
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val logFile = "/home/loic/Applications/Spark/spark-2.4.0-bin-hadoop2.7/README.md" // Should be some file on your system
+    /* val logFile = "/home/loic/Documents/M2/SID/A201712_partial.csv" // Should be some file on your system
+     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+     val logData = spark.read.textFile(logFile).cache()
+     val numAs = logData.filter(line => line.contains("-12")).count()
+     val numBs = logData.filter(line => line.contains("b")).count()
+     println(s"Lines with -12: $numAs, Lines with b: $numBs")
+     spark.stop()
+   */
+    val logFile = "/home/loic/Documents/M2/SID/A201712_partial_500000.csv"
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+  import spark.implicits._
     val logData = spark.read.textFile(logFile).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println(s"Lines with a: $numAs, Lines with b: $numBs")
-    spark.stop()
+    val subsetAmount = logData.map(line => line.split(";"))
+                  .filter(stringArray => stringArray(32) != "0")
+                  .count()
+     println(s"subset size: $subsetAmount")
+    spark.stop();
   }
 }
