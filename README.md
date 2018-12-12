77a9fdeaf3e00458acbb7c4495c6e96d0f521438
loic.stevens <loic.stevens.etu@univ-lille.fr>
2018-12-12 11:11:42 +0100

    Coucou romain

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ``` bash
 	sbt package
 	spark-submit --class "SimpleApp" --master local[4] A201712_partial.csv #LOCAL
-	spark-submit --deploy-mode cluster --class "SimpleApp" target/scala-2.11/tp1_2.11-1.0.jar hdfs://spark-1:9000/user/centos/A201712.csv
+	spark-submit --deploy-mode cluster --class "SimpleApp" target/scala-2.11/tp1_2.11-1.0.jar hdfs://spark-1:9000/user/centos/A201712.csv hdfs://spark-1:9000/user/centos/outfile
+	hadoop fs -put A201712.csv hdfs://spark-1:9000/user/centos
+	hdfs dfs -get hdfs://spark-1:9000/user/centos/outfile .
 ```
--- src/main/scala/sid/SimpleApp.scala
+++ src/main/scala/sid/SimpleApp.scala
@@ -1,16 +1,22 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode}
 
+    case class Message(message: String)
 object SimpleApp {
   def main(args: Array[String]) {
     //val logFile = "/home/loic/Documents/M2/SID/A201712_partial_500000.csv"
     val logFile = args(0)
+    val outFile = args(1)
     val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
   import spark.implicits._
+  //READ AS CSV
     val logData = spark.read.textFile(logFile).cache()
+    //ONLY GET WORK ACCIDENTS
     val subsetAmount = logData.map(line => line.split(";"))
                   .filter(stringArray => stringArray(32) != "0")
                   .count()
-     println(s"subset size: $subsetAmount")
+  val df = Seq(Message(subsetAmount.toString)).toDF().coalesce(1)
+    df.write.mode(SaveMode.Overwrite).parquet(outFile)
     spark.stop();
   }
 }
